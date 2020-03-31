b38be052d2e67dfb5737bf09d289f7ef8c71e6fa
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-01 01:22:24 +0530

    added covid

--- index.js
+++ index.js
@@ -1,61 +1,81 @@
 const TelegramBot = require('node-telegram-bot-api');
 const greet = require("./modules/greet");
+const covid = require("./modules/covid");
 const G = require('gizoogle');
 const cron = require("node-cron");
 const token = '944880131:AAGtLEWa_IIRU4c6C8F13sSdfcOMY6xn4Io';
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const resp = match[1]; 
-  
-    bot.sendMessage(chatId, resp);
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+//gangsta
+bot.onText(/\/gangsta (.+)/, (msg, match) => {
+  const resp = match[1];
+  G.string(resp, function (error, translation) {
+    bot.sendMessage(msg.chat.id, translation);
   });
-  bot.onText(/\/gangsta (.+)/, (msg, match) => {
-    const resp = match[1]; 
-    G.string(resp, function(error, translation) {
-      bot.sendMessage(msg.chat.id, translation);
-    });
-  
-  });
+
+});
+
+// //covid
+// let url="https://api.covid19india.org/data.json";
+
+// bot.onText(/\/covid (.+)/, async (msg, match) => {
+//   const resp = match[1]; 
+//   let args = msg.text.split(" ");
+//   let out=" "+args[1]+"hjsh"+args[2]+"hjsh"+args[3]+"hjsh";
+//   await bot.sendMessage(msg.chat.id, out);
 
 
-  bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    greet(bot, msg);
-    var bye = "bye";
-    //on bye 
-    if (msg.text.toString().toLowerCase().includes(bye)) {
-     bot.sendMessage(msg.chat.id, "One man down! Bye");
-     } 
+// });
 
-     //on mess
-     if (msg.text.toString().toLowerCase().includes("mess")) {
 
-     bot.sendMessage(msg.chat.id, "I hear mess! Sure you wanna go to mess ?", {
-            "reply_markup": {
-                "keyboard": [["Gate 1", "Gate 3"],   ["Shipra"], ["Mc Donalds"],["Zomato"],["Mess"]]
-                }
-            });        
-    }
-    //on reply
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  greet(bot, msg);
+  if (msg.text) {
+    covid(bot, msg);
+  }
 
-    const rep="I hear mess! Sure you wanna go to mess ?"
-    if (msg.text.toString().toLowerCase().includes("bc")) {
-         bot.sendPhoto(msg.chat.id, "/home/shubhankar/Desktop/Raamubot/static/abeysale.jpeg"); 
+
+
+  //on bye 
+  if (msg.text.toString().toLowerCase().includes("bye")) {
+    bot.sendMessage(msg.chat.id, "One man down! Bye");
+  }
+
+  //on mess
+  if (msg.text.toString().toLowerCase().includes("mess")) {
+
+    bot.sendMessage(msg.chat.id, "I hear mess! Sure you wanna go to mess ?", {
+      "reply_markup": {
+        "keyboard": [["Gate 1", "Gate 3"], ["Shipra"], ["Mc Donalds"], ["Zomato"], ["Mess"]]
       }
+    });
+  }
+  //on reply
+
+  const rep = "I hear mess! Sure you wanna go to mess ?"
+  if (msg.text.toString().toLowerCase().includes("bc")) {
+    bot.sendPhoto(msg.chat.id, "/home/shubhankar/Desktop/Raamubot/static/abeysale.jpeg");
+  }
 
 
 
-      if (msg.text.toString().toLowerCase().includes("bunk")){
-      bot.sendPoll(msg.chat.id, "I hear mass bunk! Let's see what others have to say.", ["You son of a bitch , I'm in!","short atttendance"]);}
+  if (msg.text.toString().toLowerCase().includes("bunk")) {
+    bot.sendPoll(msg.chat.id, "I hear mass bunk! Let's see what others have to say.", ["You son of a bitch , I'm in!", "short atttendance"]);
+  }
 
-  });
+});
 
-  bot.on('new_chat_members', (msg) => {
-  cron.schedule("30 21 * * 1-7 ", ()=>
-  {bot.sendMessage(msg.chat.id, msg.chat.id);})
-  });
+bot.on('new_chat_members', (msg) => {
+  cron.schedule("30 21 * * 1-7 ", () => { bot.sendMessage(msg.chat.id, msg.chat.id); })
+});
 
-  bot.on("polling_error", err => console.log(err));
+bot.on("polling_error", err => console.log(err));
---
+++ modules/covid.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const covid = async (bot, msg) => {
+    let args = msg.text.split(" ");
+    const chatId = msg.chat.id;
+    if (args[0] == "/covid") {
+        bot.sendMessage(chatId, "covid running");
+        let res = await axios.get("https://api.covid19india.org/data.json");
+        let { cases_time_series } = res.data;
+        let {
+            dailyconfirmed,
+            dailydeceased,
+            dailyrecovered,
+            totalconfirmed,
+            totaldeceased,
+            totalrecovered
+        } = cases_time_series[cases_time_series.length - 1];
+        let output = " " + "Here are the stats for India : \n\nTotal confirmed cases: " + totalconfirmed + "\n\nTotal deaths: " + totaldeceased + "\n\nTotal recovered: " + totalrecovered + "\n\nNew cases today: " + dailyconfirmed + "\n\nStay home \nStay safe";
+        bot.sendMessage(chatId, output); 
+        
+        
+       //console.log(dataConfirmed);
+
+
+
+    }
+
+}
+module.exports = covid;
