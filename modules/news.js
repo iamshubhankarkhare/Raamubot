db3c1a146f5bd125016e26df0adb834aee41e278
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-14 23:45:47 +0530

    adding news final

--- modules/news.js
+++ modules/news.js
@@ -4,18 +4,20 @@ const news = async (bot, msg) => {
     let args = msg.text.split(" ");
     const chatId = msg.chat.id;
     if (args[0] == "/news") {
-        var i = Math.floor(Math.random() * 100);
         if (args[1]) {
-            
-            const res = await axios.get(`https://newsapi.org/v2/everything?q=${args[i]}&apiKey=${process.env.NEWS_API_KEY}`);
-        }
-        else{
-            const res = await axios.get(`https://newsapi.org/v2/everything?q=corona&apiKey=${process.env.NEWS_API_KEY}`);
+            var i = Math.floor(Math.random() * 100);
+            let res = await axios.get(`https://newsapi.org/v2/everything?q=${args[i]}&apiKey=${process.env.NEWS_API_KEY}`);
+            let out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
+            bot.sendMessage(msg.chat.id, out);
 
         }
-        
-        let out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
-        bot.sendMessage(msg.chat.id, out);
+        else{
+            var i = Math.floor(Math.random() * 100);
+            let res = await axios.get(`https://newsapi.org/v2/everything?q=corona&apiKey=${process.env.NEWS_API_KEY}`);
+            let out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
+            bot.sendMessage(msg.chat.id, out);
+
+        }
 
     }
 }
