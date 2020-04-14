d9b1fab62dc91975bedd87f94ef92725a2b0dec2
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-15 00:08:15 +0530

    adding news final

--- modules/news.js
+++ modules/news.js
@@ -6,7 +6,7 @@ const news = async (bot, msg) => {
     if (args[0] == "/news") {
         if (args[1]) {
             let res = await axios.get(`https://newsapi.org/v2/everything?q=${args[1]}&apiKey=${process.env.NEWS_API_KEY}`);
-            var i = (Math.floor(Math.random() * res.data.totalResults))-1;
+            var i = (Math.floor(Math.random() * 20));
             let out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
             bot.sendMessage(msg.chat.id, out);
 
