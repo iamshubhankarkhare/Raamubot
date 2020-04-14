883557a05c427f5e77a228f002895e26d279533d
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-14 23:28:00 +0530

    adding news

--- modules/news.js
+++ modules/news.js
@@ -5,14 +5,19 @@ const news = async (bot, msg) => {
     const chatId = msg.chat.id;
     if (args[0] == "/news") {
         if (args[1]) {
-       // var i=Math.floor(Math.random() * 10);
-        let res = await axios.get(`https://newsapi.org/v2/everything?q=${args[1]}&apiKey=${process.env.NEWS_API_KEY}`);
-        var out=`${res.data.articles[1].title}\n\n${res.data.articles[1].description}\n${res.data.articles[1].url}`;
-        bot.sendMessage(msg.chat.id, out );
-        bot.sendPhoto(chatId, res.data.articles[1].urlToImage, { caption: out });
+            var i = Math.floor(Math.random() * 10);
+            let res = await axios.get(`https://newsapi.org/v2/everything?q=${args[i]}&apiKey=${process.env.NEWS_API_KEY}`);
+            var out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
+            bot.sendMessage(msg.chat.id, out);
+           // bot.sendPhoto(chatId, res.data.articles[i].urlToImage, { caption: out });
 
         }
-        
+        else{
+            let res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
+            var out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
+            bot.sendMessage(msg.chat.id, out);
+        }
+
     }
 }
 module.exports = news;
