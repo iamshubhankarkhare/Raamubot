54ce6e451d89c1e9d65f3af546382fdcef5e8445
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-14 23:51:37 +0530

    adding news final

--- modules/news.js
+++ modules/news.js
@@ -6,17 +6,16 @@ const news = async (bot, msg) => {
     if (args[0] == "/news") {
         if (args[1]) {
             var i = Math.floor(Math.random() * 100);
-            let res = await axios.get(`https://newsapi.org/v2/everything?q=${args[i]}&apiKey=${process.env.NEWS_API_KEY}`);
+            let res = await axios.get(`https://newsapi.org/v2/everything?q=${args[1]}&apiKey=${process.env.NEWS_API_KEY}`);
             let out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
             bot.sendMessage(msg.chat.id, out);
 
         }
         else{
-            var i = Math.floor(Math.random() * 100);
-            let res = await axios.get(`https://newsapi.org/v2/everything?q=corona&apiKey=${process.env.NEWS_API_KEY}`);
+            var i = Math.floor(Math.random() * 10);
+            let res = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
             let out = `${res.data.articles[i].title}\n\n${res.data.articles[i].description}\n${res.data.articles[i].url}`;
             bot.sendMessage(msg.chat.id, out);
-
         }
 
     }
