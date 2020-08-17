5e8f8e0b81b6df67982f10c343fb04bd464741c3
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-08-17 14:29:47 +0530

    Change start msg

--- modules/start.js
+++ modules/start.js
@@ -1,8 +1,8 @@
 const start = async (bot, msg) => {
-    let args = msg.text.split(" ");
-    const chatId = msg.chat.id;
-    if (args[0] == "/start" || args[0] == "/heyraamu") {
-        var start = `Hi! I'm Raamu. Your quarantine partner.\nHere are a few commands to get the best out of me:\n
+  let args = msg.text.split(' ');
+  const chatId = msg.chat.id;
+  if (args[0] == '/start' || args[0] == '/heyraamu') {
+    var start = `Hi! I'm Raamu. Your quarantine partner.\nHere are a few commands to get the best out of me:\n
   /covid : I'll provide you the current covid stats of india\n
   /movie <movie title> : Get details about the movie\n
   /gangsta <sentence>: I be bout ta translate yo' lyrics ta gangsta\n
@@ -10,13 +10,11 @@ const start = async (bot, msg) => {
   /showclass : I'm keeping a track of all these classes\n
   /facts : I'll hit you with a useless fact\n
   /news <topic> : Know what's up with the world\n
+  /timetable : What's today's timetable\n
+  /remindtimetable : What is next class\n
   I got more features, let'em be a surprise though XD`;
-        
-        bot.sendMessage(msg.chat.id, start);
 
-
-
-    }
-
-}
+    bot.sendMessage(msg.chat.id, start);
+  }
+};
 module.exports = start;
